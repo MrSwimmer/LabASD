147577242f8f445d9607bb09f089db143d5febac
MrSwimmer <sevastian0902@gmail.com>
2018-09-30 23:42:32 +0700

    add fucking iterator

--- tree/Tree.h
+++ tree/Tree.h
@@ -5,6 +5,11 @@
 #ifndef LAB_TREE_H
 #define LAB_TREE_H
 
+#define EMPTY_TREE_ERR 1
+#define INVALID_INDEX_ERR 2
+#define ITERATOR_END_ERR 3
+#define ITERATOR_BEGIN_ERR 4
+
 #include <iostream>
 
 using namespace std;
@@ -25,27 +30,92 @@ class Tree {
         }
     };
 
-public:
+    class Iterator {
+    public:
+        Node *node;
+        Tree *tree;
 
-    Tree() {
-    }
+        Iterator(Tree *t) : tree(t) {
+
+        }
+
+        Iterator *first() {
+            if(tree->isEmpty())
+                throw EMPTY_TREE_ERR;
+            //do it
+        }
+
+        Iterator *last() {
+            if(tree->isEmpty())
+                throw EMPTY_TREE_ERR;
+            //do it
+        }
+
+        Iterator &operator--(int) {
+            if(tree->isEmpty()) throw EMPTY_TREE_ERR;
+            //do it
+            return *this;
+        };
+
+        Iterator &operator++(int) {
+            if(tree->isEmpty()) throw EMPTY_TREE_ERR;
+            //do it
+            return *this;
+        };
+
+        bool isInstalled() {
+            return !this->node == NULL;
+        }
+
+        T read(){
+            if(tree->isEmpty()) throw EMPTY_TREE_ERR;
+            return node->data;
+        }
+
+        void write(T data){
+            if(tree->isEmpty()) throw EMPTY_TREE_ERR;
+            node->data = data;
+        }
+    };
+
+public:
 
     void print() {
         printNode(root);
     }
 
-
     void add(T data) {
         addNode(root, data);
+    }
+
+    bool isEmpty() {
+        return root == NULL;
+    }
+
+    Node *get(T data) {
+
+    }
+
+    int size() {
+        count = 0;
+        printNode(root);
+        return count;
+    }
+
+    virtual ~Tree() {
+
     }
 
 private:
 
     Node *root = NULL;
 
+    int count = 0;
+
     void printNode(Node *node) {
         if (node->left != NULL)
             printNode(node->left);
+        count++;
         cout << node->data << endl;
         if (node->right != NULL)
             printNode(node->right);
@@ -70,6 +140,10 @@ private:
                 }
             }
         }
+    }
+
+    void clear() {
+        //do it
     }
 };
 
